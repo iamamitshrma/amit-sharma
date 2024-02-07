dd015a837b8773c7eae36c32375ea926c28ef340
Amit Sharma <iamamitshrma@gmail.com>
2024-02-07 16:58:50 +0530

    hero section added

--- tailwind.config.js
+++ tailwind.config.js
@@ -23,18 +23,20 @@ module.exports = {
     },
     extend: {
       colors: {
-        active: "#8b169f"
+        active: "#8b169f",
+        lightGray: "#444",
+        darkGray: "#212529"
       },
-      keyframes: {
-        "accordion-down": {
-          from: { height: "0" },
-          to: { height: "var(--radix-accordion-content-height)" },
-        },
-        "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: "0" },
-        },
-      },
+      // keyframes: {
+      //   "accordion-down": {
+      //     from: { height: "0" },
+      //     to: { height: "var(--radix-accordion-content-height)" },
+      //   },
+      //   "accordion-up": {
+      //     from: { height: "var(--radix-accordion-content-height)" },
+      //     to: { height: "0" },
+      //   },
+      // },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
